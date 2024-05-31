89dd9982aad67de891fcd2a3b8d1de07f49f49b8
pzheng460 <lacrimosaofdana@hotmail.com>
2024-05-30 21:02:02 -0400

    version 2.0

--- main.cpp
+++ main.cpp
@@ -1,13 +1,96 @@
-#include <iostream>
-#include "tgaimage.h"
+#include <limits>
+#include <filesystem>
+#include "model.h"
+#include "our_gl.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
+constexpr int width  = 800; // output image size
+constexpr int height = 800;
+constexpr vec3 light_dir{1,1,1}; // light source
+constexpr vec3       eye{1,1,3}; // camera position
+constexpr vec3    center{0,0,0}; // camera direction
+constexpr vec3        up{0,1,0}; // camera up vector
 
-int main(int argc, char **argv) {
-    TGAImage image(100, 100, TGAImage::RGB); // 100*100的图像，每像素三个字节存储RGB值
-    image.set(52, 41, red); // 把（52，41）位置的像素设置为红色
-    image.flip_vertically(); // 我希望原点在左下角，所以翻转一下
-    image.write_tga_file("output.tga"); // 写入文件
+extern mat<4,4> ModelView; // "OpenGL" state matrices
+extern mat<4,4> Projection;
+
+struct Shader : IShader {
+    const Model &model;
+    vec3 uniform_l;       // light direction in view coordinates
+    mat<2,3> varying_uv;  // triangle uv coordinates, written by the vertex shader, read by the fragment shader
+    mat<3,3> varying_nrm; // normal per vertex to be interpolated by FS
+    mat<3,3> view_tri;    // triangle in view coordinates
+
+    Shader(const Model &m) : model(m) {
+        uniform_l = proj<3>((ModelView*embed<4>(light_dir, 0.))).normalized(); // transform the light vector to view coordinates
+    }
+
+    virtual void vertex(const int iface, const int nthvert, vec4& gl_Position) {
+        varying_uv.set_col(nthvert, model.uv(iface, nthvert));
+        varying_nrm.set_col(nthvert, proj<3>((ModelView).invert_transpose()*embed<4>(model.normal(iface, nthvert), 0.)));
+        gl_Position= ModelView*embed<4>(model.vert(iface, nthvert));
+        view_tri.set_col(nthvert, proj<3>(gl_Position));
+        gl_Position = Projection*gl_Position;
+    }
+
+    virtual bool fragment(const vec3 bar, TGAColor &gl_FragColor) {
+        vec3 bn = (varying_nrm*bar).normalized(); // per-vertex normal interpolation
+        vec2 uv = varying_uv*bar; // tex coord interpolation
+
+        // for the math refer to the tangent space normal mapping lecture
+        // https://github.com/ssloy/tinyrenderer/wiki/Lesson-6bis-tangent-space-normal-mapping
+        mat<3,3> AI = mat<3,3>{ {view_tri.col(1) - view_tri.col(0), view_tri.col(2) - view_tri.col(0), bn} }.invert();
+        vec3 i = AI * vec3{varying_uv[0][1] - varying_uv[0][0], varying_uv[0][2] - varying_uv[0][0], 0};
+        vec3 j = AI * vec3{varying_uv[1][1] - varying_uv[1][0], varying_uv[1][2] - varying_uv[1][0], 0};
+        mat<3,3> B = mat<3,3>{ {i.normalized(), j.normalized(), bn} }.transpose();
+
+        vec3 n = (B * model.normal(uv)).normalized(); // transform the normal from the texture to the tangent space
+        double diff = std::max(0., n*uniform_l); // diffuse light intensity
+        vec3 r = (n*(n*uniform_l)*2 - uniform_l).normalized(); // reflected light direction, specular mapping is described here: https://github.com/ssloy/tinyrenderer/wiki/Lesson-6-Shaders-for-the-software-renderer
+        double spec = std::pow(std::max(-r.z, 0.), 5+sample2D(model.specular(), uv)[0]); // specular intensity, note that the camera lies on the z-axis (in view), therefore simple -r.z
+
+        TGAColor c = sample2D(model.diffuse(), uv);
+        for (int i : {0,1,2})
+            gl_FragColor[i] = std::min<int>(10 + c[i]*(diff + spec), 255); // (a bit of ambient light, diff + spec), clamp the result
+
+        return false; // the pixel is not discarded
+    }
+};
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
+        return 1;
+    }
+
+    std::vector<std::string> obj_files;
+
+    for (int i = 1; i < argc; i++) {
+        auto directory_path = std::filesystem::path(argv[i]);
+        // 迭代目录中的所有文件，寻找.obj文件
+        for (const auto& entry : std::filesystem::directory_iterator(directory_path)) {
+            if (entry.is_regular_file() && entry.path().extension() == ".obj") {
+                obj_files.push_back(entry.path().string());
+            }
+        }
+    }
+
+    TGAImage framebuffer(width, height, TGAImage::RGB); // the output image
+    lookat(eye, center, up);                            // build the ModelView matrix
+    viewport(width/8, height/8, width*3/4, height*3/4); // build the Viewport matrix
+    projection((eye-center).norm());                    // build the Projection matrix
+    std::vector<double> zbuffer(width*height, std::numeric_limits<double>::max());
+
+    for (const auto& obj_file : obj_files) { // iterate through all input objects
+        Model model(obj_file.c_str());
+        Shader shader(model);
+        for (int i = 0; i < model.nfaces(); i++) { // for every triangle
+            vec4 clip_vert[3]; // triangle coordinates (clip coordinates), written by VS, read by FS
+            for (int j = 0; j < 3; j++) // for every vertex of the triangle
+                shader.vertex(i, j, clip_vert[j]); // call the vertex shader for each triangle vertex
+            triangle(clip_vert, shader, framebuffer, zbuffer); // actual rasterization routine call
+        }
+    }
+    framebuffer.write_tga_file("framebuffer.tga");
     return 0;
 }
+
--- tgaimage.h
+++ tgaimage.h
@@ -1,98 +1,51 @@
-#ifndef REINRENDERER_TGAIMAGE_H
-#define REINRENDERER_TGAIMAGE_H
-
+#pragma once
+#include <cstdint>
 #include <fstream>
+#include <vector>
 
-#pragma pack(push,1) // 将默认的变量对齐方式推入堆栈，设置新的对齐系数为1
-// TGA头文件结构
-struct TGA_Header {
-    char idlength; // 本字段是1字节无符号整型，指出图像信息字段长度，其取值范围是0到255，当它为0时表示没有图像的信息字段。
-    char colormaptype; // 颜色表类型（1字节）:0表示没有颜色表，1表示颜色表存在。由于本格式是无颜色表的，因此此项通常被忽略。
-    char datatypecode; //图像类型码:2:非压缩RGB格式 10:压缩RGB格式
-    short colormaporigin; // 颜色表首址:颜色表首的入口索引，整型（低位-高位）
-    short colormaplength; // 颜色表长度:颜色表的表项总数，整型（低位-高位）
-    char colormapdepth; // 颜色表项位数:位数（bit），16代表 16位 TGA，24代表 24位 TGA，32代表 32位 TGA
-    short x_origin; // 图像X坐标的起始位置:图像左下角X坐标的整型（低位-高位）值
-    short y_origin; // 图像Y坐标的起始位置:图像左下角Y坐标的整型（低位-高位）值
-    short width; // 图像宽度:以像素为单位，图像宽度的整型（低位-高位）
-    short height; // 图像高度:以像素为单位，图像高度的整型（低位-高位）
-    char bitsperpixel; // 图像每像素存储占用位数(bpp):它的值为16，24或 32等等。决定了该图像是 TGA 16，TGA24,TGA 32等等。
-    char imagedescriptor; // 图像描述符字节
+#pragma pack(push,1)
+struct TGAHeader {
+    std::uint8_t  idlength = 0;
+    std::uint8_t  colormaptype = 0;
+    std::uint8_t  datatypecode = 0;
+    std::uint16_t colormaporigin = 0;
+    std::uint16_t colormaplength = 0;
+    std::uint8_t  colormapdepth = 0;
+    std::uint16_t x_origin = 0;
+    std::uint16_t y_origin = 0;
+    std::uint16_t width = 0;
+    std::uint16_t height = 0;
+    std::uint8_t  bitsperpixel = 0;
+    std::uint8_t  imagedescriptor = 0;
 };
-#pragma pack(pop) //恢复默认的变量对齐方式
-//上述结构体的大小是18字节
+#pragma pack(pop)
 
-//TGAColor类
 struct TGAColor {
-    //union大小为4字节
-    union {
-        struct {
-            unsigned char b, g, r, a;
-        };//RGBA值
-        unsigned char raw[4];
-        unsigned int val;
-    };
-    int bytespp; //图像每像素存储占用字节数
-
-    TGAColor() : val(0), bytespp(1) {
-    }
-
-    TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A) : b(B), g(G), r(R), a(A), bytespp(4) {
-    }
-
-    TGAColor(int v, int bpp) : val(v), bytespp(bpp) {
-    }
-
-    TGAColor(const TGAColor &c) : val(c.val), bytespp(c.bytespp) {
-    }//拷贝构造函数
-
-    TGAColor(const unsigned char *p, int bpp) : val(0), bytespp(bpp) {
-        for (int i = 0; i < bpp; i++) {
-            raw[i] = p[i];
-        }
-    } //构造函数
-
-    TGAColor & operator =(const TGAColor &c) {
-        if (this != &c) {
-            bytespp = c.bytespp;
-            val = c.val;
-        }
-        return *this;
-    } // 赋值函数
+    std::uint8_t bgra[4] = {0,0,0,0};
+    std::uint8_t bytespp = 4;
+    std::uint8_t& operator[](const int i) { return bgra[i]; }
 };
 
+struct TGAImage {
+    enum Format { GRAYSCALE=1, RGB=3, RGBA=4 };
 
-class TGAImage {
-protected:
-    unsigned char* data;
-    int width; // 图像的宽度
-    int height; // 图像的高度
-    int bytespp; // 图像的每像素字节数
+    TGAImage() = default;
+    TGAImage(const int w, const int h, const int bpp);
+    bool  read_tga_file(const std::string filename);
+    bool write_tga_file(const std::string filename, const bool vflip=true, const bool rle=true) const;
+    void flip_horizontally();
+    void flip_vertically();
+    TGAColor get(const int x, const int y) const;
+    void set(const int x, const int y, const TGAColor &c);
+    int width()  const;
+    int height() const;
+private:
+    bool   load_rle_data(std::ifstream &in);
+    bool unload_rle_data(std::ofstream &out) const;
 
-    bool load_rle_data(std::ifstream &in);
-    bool unload_rle_data(std::ofstream &out);
-public:
-    enum Format {
-        GRAYSCALE=1, RGB=3, RGBA=4
-    };//图像格式：灰度图，RGB图，RGBA图
-
-    TGAImage();
-    TGAImage(int w, int h, int bpp);
-    TGAImage(const TGAImage &img);
-    bool read_tga_file(const char *filename);
-    bool write_tga_file(const char *filename, bool rle=true);
-    bool flip_horizontally();
-    bool flip_vertically();
-    bool scale(int w, int h);
-    TGAColor get(int x, int y); // 获取（x,y）位置处像素的颜色值
-    bool set(int x, int y, TGAColor c); // 将（x,y）位置处的像素设置为颜色c
-    ~TGAImage();
-    TGAImage & operator = (const TGAImage &img); // 赋值函数
-    int get_width(); // 获取图像宽度
-    int get_height(); // 获取图像高度
-    int get_bytespp(); // 获取图像每像素字节数
-    unsigned char *buffer(); // 获取图像数据指针
-    void clear();
+    int w = 0;
+    int h = 0;
+    std::uint8_t bpp = 0;
+    std::vector<std::uint8_t> data = {};
 };
 
-#endif //REINRENDERER_TGAIMAGE_H
